001c62fe049e06e0c08d3b8721978b236a563886
Zubin <zeuslawyer@users.noreply.github.com>
2019-09-10 21:54:03 +1000

    deleting secrets

--- src/config/dev.secrets.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBfQoCNs3BavQ95uCsnlMcSn_xMi9oB25U',
-  authDomain: 'myuniversalserver.firebaseapp.com',
-  databaseURL: 'https://myuniversalserver.firebaseio.com',
-  projectId: 'myuniversalserver',
-  storageBucket: 'myuniversalserver.appspot.com',
-  messagingSenderId: '586045105002',
-  appId: '1:586045105002:web:6a5d8d690b0faa5c9cbb13'
-};
